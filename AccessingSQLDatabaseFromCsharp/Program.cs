7a768db2bcd1604232bf3eb91d1ae70949e10938
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-08-14 17:11:38 +0200

    Add project files.

---
+++ AccessingSQLDatabaseFromCsharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+namespace AccessingSQLDatabaseFromCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                TestQueries().Wait();
+        }
+
+        static async Task TestQueries()
+        {
+            var connStr = @"Data Source=(localdb)\local;Initial Catalog=AccessingSQLDatabaseFromCsharp;Integrated Security=True;";
+            var conn = new SqlConnection(connStr);
+
+            var readMany = @"SELECT Id, FirstName, LastName, BirthYear 
+                             FROM Person";
+
+            // DECLARE @Id int = 1;
+            var readOne = @"SELECT Id, FirstName, LastName, BirthYear
+                            FROM Person
+                            WHERE Id = @Id";
+
+            var readOneByName = @"SELECT Id, FirstName, LastName, BirthYear
+                                  FROM Person
+                                  WHERE FirstName = @FirstName";
+
+            var create = @"INSERT INTO Person (FirstName, LastName, BirthYear)
+                           VALUES (@FirstName, @LastName, @BirthYear)";
+
+            var delete = @"DELETE FROM Person WHERE Id = @Id";
+
+            var deleteAll = @"DELETE FROM Person";
+
+            var update = @"UPDATE Person 
+                           SET LastName = @LastName, FirstName = @FirstName
+                           WHERE Id = @Id";
+
+            int rowsDeleted = await conn.ExecuteAsync(deleteAll);
+
+            int rowsInserted1 = await conn.ExecuteAsync(create, new { FirstName = "Audun", LastName = "Nicolaisen", BirthYear = 2001 });
+            int rowsInserted2 = await conn.ExecuteAsync(create, new { FirstName = "Per", LastName = (string)null, BirthYear = 1980 });
+            int rowsInserted3 = await conn.ExecuteAsync(create, new { FirstName = "Pål", LastName = (string)null, BirthYear = 1981 });
+
+            IEnumerable<Person> persons = await conn.QueryAsync<Person>(readMany);
+
+            Person audun = await conn.QueryFirstOrDefaultAsync<Person>(readOneByName, new { FirstName = "Audun" });
+
+            int rowsAffected2 = await conn.ExecuteAsync(update, new { FirstName = "Petter", LastName = "Pettersen", Id = audun.Id});
+            persons = await conn.QueryAsync<Person>(readMany);
+
+            int rowsAffected3 = await conn.ExecuteAsync(delete, new { Id = audun.Id });
+            persons = await conn.QueryAsync<Person>(readMany);
+
+        }
+
+
+    }
+}
---
+++ AccessingSQLDatabaseFromCsharp/queries.sql
@@ -0,0 +1,43 @@
+﻿﻿-- Read - mange											-- CRUD = Create Read Update Delete
+SELECT Id, FirstName, LastName, BirthYear
+FROM Person
+-- WHERE BirthYear > 1980
+
+-- Read - én
+SELECT Id, FirstName, LastName, BirthYear
+FROM Person
+WHERE Id = 1001
+
+-- Read - én - med parameter
+DECLARE @Id int = 1;
+SELECT Id, FirstName, LastName, BirthYear
+FROM Person
+WHERE Id = @Id
+
+-- Create
+DECLARE @FirstName varchar(max) = 'Audun';
+DECLARE @LastName varchar(max) = 'Nicolaisen';
+DECLARE @BirthYear int = 2001;
+INSERT INTO Person (FirstName, LastName, BirthYear)
+VALUES (@FirstName, @LastName, @BirthYear)
+
+-- Delete
+DECLARE @Id int = 1;
+DELETE FROM Person WHERE Id = @Id
+
+-- Update
+DECLARE @FirstName varchar(max) = 'Petter';
+DECLARE @LastName varchar(max) = 'Pettersen';
+DECLARE @Id int = 1;
+UPDATE Person 
+SET LastName = @LastName, FirstName = @FirstName
+WHERE Id = @Id
+-- WHERE BirthYear > @BirthYear
+
+
+CREATE TABLE [dbo].[Person](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](max) NULL,
+	[LastName] [nvarchar](max) NULL,
+	[BirthYear] [int] NULL
+) 
